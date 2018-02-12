d3bab249091069ca17f80f6f7fa148e3a20574d7
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2018-02-11 20:03:51 -0500

    04 (#4)
    * 04
    Basic installation of angular-aria, angular-anime and angular-material . Correction to first install
    
    * Angular material and two buttons in the middle

---
+++ bower-01.bat
@@ -0,0 +1,8 @@
+cd public
+start bower install angular
+pause
+start bower install firebase
+pause
+@echo Select Option 2
+start bower install angularfire
+cd ..\
--- bowerInstall.bat
+++
@@ -1,7 +0,0 @@
-cd public
-bower angular
-pause
-bower firebase
-pause
-@echo Select Option 2
-bower angularfire
