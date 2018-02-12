0b925def23764cd24136ac8930299af5b16c4f9d
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2018-02-11 20:02:00 -0500

    Angular material and two buttons in the middle

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 (function(){
 
-    var asaoApp = angular.module('asaoApp', ['firebase']);
+    var asaoApp = angular.module('asaoApp', ['firebase', 'ngMaterial']);
 
     asaoApp.controller('mainCtrl', ['$scope', function($scope){
         console.log('Estamos Adentro de Firebase!');
