251fc1fd284e0d6aff22af899485c7a67fde2d75
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2018-02-11 22:35:16 -0500

    Reference to DB and it shows a dialof¿g for "Name" (#5)
    We build the first Dialog window to get the BBQ name, also we get the reference to DB with normal Javascript normal and stored in a  variable

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,28 @@
 
     var asaoApp = angular.module('asaoApp', ['firebase', 'ngMaterial']);
 
-    asaoApp.controller('mainCtrl', ['$scope', function($scope){
-        console.log('Estamos Adentro de Firebase!');
+    asaoApp.controller('mainCtrl', ['$scope', '$firebaseArray', '$firebaseObject', '$mdDialog',
+			function($scope, $firebaseArray, $firebaseObject, $mdDialog){
+				var vm = this;
+				var ref = firebase.database().ref().child("asado-app-20170209"); // referencia a los asados
+				vm.nuevoAsado = function () {
+					
+					var confirm = $mdDialog.prompt()
+                        .title('¿Cuál es el nombre del asado?')
+                        .placeholder('Asao Peruano!')
+                        .targetEvent()
+                        .ok('Seguir')
+                        .cancel('Me arrepentí');
+					
+					$mdDialog.show(confirm).then(function (result) {
+						console.log('el asado se va a llamar', result);
+            }, function () {
+						console.log('se arrepintió');
+					});
+					
+					vm.asadosArray = $firebaseArray(ref);
+					
+				}
     }]);
 
 })();
