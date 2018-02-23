29e368911f0fe5adb7aa0a6f7a2a72c224e467e1
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2018-02-22 21:46:33 -0500

    Saving a Gest (Paricipante) of right way: Step 10, Better implementation of Method to store a new Gest of Roast, taking a new options and finally we do using "Save" and "Loaded" on the Object

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,32 @@
 (function(){
 
-    var asaoApp = angular.module('asaoApp', ['firebase', 'ngMaterial']);
+  var asaoApp = angular.module('asaoApp', ['firebase', 'ngMaterial']);
 
-    asaoApp.controller('mainCtrl', ['$scope', '$firebaseArray', '$firebaseObject', '$mdDialog',
-			function($scope, $firebaseArray, $firebaseObject, $mdDialog){
-				var vm = this;
-				var ref = firebase.database().ref().child("asado-app-20170209"); // referencia a los asados
-				vm.nuevoAsado = function () {
+  asaoApp.controller('mainCtrl', ['$scope', '$firebaseArray', '$firebaseObject', '$mdDialog',
+      function($scope, $firebaseArray, $firebaseObject, $mdDialog){
+        var vm = this;
+        var ref = firebase.database().ref().child("asado-app-20170209"); // referencia a los asados
+        vm.nuevoAsado = function () {
+
+          var confirm = $mdDialog.prompt()
+                          .title('¿Cuál es el nombre del asado?')
+                          .placeholder('Asao Peruano!')
+                          .targetEvent()
+                          .ok('Seguir')
+                          .cancel('Me arrepentí');
+
+          $mdDialog.show(confirm).then(function (nombre) {
+            crearAsado(nombre);
+          }, function () {
+            console.log('se arrepintió');
+          });
 					
-					var confirm = $mdDialog.prompt()
-                        .title('¿Cuál es el nombre del asado?')
-                        .placeholder('Asao Peruano!')
-                        .targetEvent()
-                        .ok('Seguir')
-                        .cancel('Me arrepentí');
-					
-					$mdDialog.show(confirm).then(function (nombre) {
-						crearAsado(nombre);
-            }, function () {
-						console.log('se arrepintió');
-					});
-					
-					function crearAsado(nombre){
+        function crearAsado(nombre){
 						if (nombre && nombre.length) {
-								vm.asadosArray = $firebaseArray(ref);
-								vm.asadosArray.$add({
-										nombre: nombre
-								});
+              vm.asadosArray = $firebaseArray(ref);
+              vm.asadosArray.$add({
+                nombre: nombre
+              });
 
 								// vm.asadosArray = $firebaseObject(ref);
 								// vm.asadosArray.edad = nombre;
@@ -36,17 +36,17 @@
 					
 				};
 				
-				vm.verAsados = function() {
-					vm.isAsadosDisplayed = true;
-					vm.isDetailsDisplayed = false;
-					vm.asadosArray = $firebaseArray(ref);
-					vm.asadosObject = $firebaseObject(ref);
+        vm.verAsados = function() {
+          vm.isAsadosDisplayed = true;
+          vm.isDetailsDisplayed = false;
+          vm.asadosArray = $firebaseArray(ref);
+          vm.asadosObject = $firebaseObject(ref);
 				};
 				
 				vm.verDetalles = function(asado) {
-					vm.isAsadosDisplayed = false;
-					vm.isDetailsDisplayed = true;
-					vm.asadoActual = asado;
+          vm.isAsadosDisplayed = false;
+          vm.isDetailsDisplayed = true;
+          vm.asadoActual = asado;
 					asado.haSidoVisto = true;
 					vm.asadosArray.$save(asado);
 				};
@@ -60,13 +60,26 @@
 							.cancel('Me arrepentí');
 
 					$mdDialog.show(confirm).then(function (nombre) {
-						vm.asadoActual.participantes.push({
-								nombre: nombre
-						});
-					}, function () {
-							console.log('se arrepintió');
-					});
+						var refParticipantes = ref.child(vm.asadoActual.$id);
+            var asado = $firebaseObject(refParticipantes);
+            asado.$loaded().then(() => {
+              if (!asado.participantes) {
+                asado.participantes = [
+                  {
+                    nombre: nombre
+                  }
+                ];
+                } else {
+                asado.participantes.push({
+                  nombre: nombre
+                });
+              }
+              asado.$save();
+            });
+          }, function () {
+            console.log('se arrepintió');
+          });
         }
-    }]);
+      }]);
 
 })();
